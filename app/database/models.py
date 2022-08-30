0115a9d2f4022a9aa726610f4f958f2b8f0f8242
Ajit Kumar <ajit.kumar@1silverbullet.tech>
2022-08-30 14:52:05 +0530

    unwanted file

--- app/database/models.py
+++ app/database/models.py
@@ -20,14 +20,3 @@ class Students(Base):
         return 'studentsDetails(name=%s, classs=%s,rollNo=%s, MobileNo=%s, address=%s)' % (self.name, self.classs,self.rollNo, self.MobileNo, self.address)
     
     
-class Staff(Base):
-    __tablename__ = "staffs"
-    
-    id = Column(Integer, primary_key=True,index=True)
-    name = Column(String(80), nullable=False)
-    gender= Column(Enum('male', 'female'))
-    MobileNo = Column(VARCHAR(32))
-    address = Column(String(200))
-    
-    def __repr__(self):
-        return 'staffDetails(name=%s, gender=%s, MobileNo=%s, address=%s)' % (self.name,self.gender, self.MobileNo, self.address)
